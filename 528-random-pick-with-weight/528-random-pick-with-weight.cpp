532dfd864eb35b6888ae2303323d647fc445035e
yzy2 <54012454+yzy2@users.noreply.github.com>
2022-01-07 16:10:08 -0500

    Time: 76 ms (85.88%), Space: 40.5 MB (17.81%) - LeetHub

---
+++ 528-random-pick-with-weight/528-random-pick-with-weight.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> m_ct;
+    Solution(vector<int>& w) {
+        m_ct = {0}; 
+        for(int i =0; i < w.size(); i++){
+            m_ct.push_back(m_ct[i] + w[i]);
+        }
+    }
+    
+    int pickIndex() {
+        int target = rand()%m_ct[m_ct.size()-1];
+        
+        // bfs Search 
+        int beg, end, mid;
+        beg = 0;
+        end = m_ct.size()-1;
+        while(beg < end){ // exclude m_ct[end] 
+            mid = beg + (end-beg)/2;
+            
+            if(m_ct[mid] == target){
+                return mid;
+            }
+            else if(target > m_ct[mid]){
+                // because always exclude end, no bounds check needed
+                if(target < m_ct[mid+1]){
+                    return mid;
+                }
+                beg = mid+1;
+                
+            }else{
+                // always excluding end so mid instead of mid -1
+                end = mid;
+            }
+            
+        }
+        
+        return 0;
+    }
+};
