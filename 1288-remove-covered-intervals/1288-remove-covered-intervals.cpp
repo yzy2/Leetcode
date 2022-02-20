de752e0886eb96711033a7971f3202b786e66864
yzy2 <54012454+yzy2@users.noreply.github.com>
2022-02-20 15:15:16 -0500

    Time: 168 ms (9.11%), Space: 12.5 MB (18.44%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -7,7 +7,7 @@ public:
         sort(intervals.begin(), intervals.end(), comp);
         
         unordered_set<int> hset;
-        int output = 0;
+        int output = intervals.size();
         for(int i = 0; i < intervals.size()-1; i++){
             for(int j = i+1; j < intervals.size(); j++){
                 if(hset.find(j) != hset.end()){
@@ -15,11 +15,11 @@ public:
                 }
                 else if(intervals[i][0] <= intervals[j][0] && intervals[i][1] >= intervals[j][1]){
                     hset.insert(j);
-                    output++;
+                    output--;
                 }     
             }
 
         }
-        return intervals.size() - output;
+        return output;
     }
 };
